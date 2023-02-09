304f5cfffbe40c4b352fae38c3cb2b5580cf70e3
M. A. Startsev <m.a.startsev@gmail.com>
2023-02-10 01:05:34 +0200

    rename container

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,14 +2,14 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryName = 'gallery';
-const galleryEl = document.querySelector(`.$ galleryName}`);
+const galleryEl = document.querySelector(`.${galleryName}`);
 
 const createGallery = ({ gallery, galleryName }) =>
   (gallery.innerHTML = galleryItems.reduce(
     (arr, { preview, original, description }) =>
       `${arr}\n
-        <a class='$ galleryName}__item $ galleryName}__link' href='${original}'>
-            <img class='$ galleryName}__image' src='${preview}' alt='${description}' width='320px'>
+        <a class='${galleryName}__item ${galleryName}__link' href='${original}'>
+            <img class='${galleryName}__image' src='${preview}' alt='${description}' width='320px'>
         </a>
     `,
     ''
@@ -19,7 +19,7 @@ createGallery({ gallery: galleryEl, galleryName: galleryName });
 
 galleryEl.onclick = e => {
   e.preventDefault();
-  if (!e.target.classList.contains(`$ galleryName}__image`)) return;
+  if (!e.target.classList.contains(`${galleryName}__image`)) return;
 
   const modal = basicLightbox.create(
     `<img width="1400" height="900" src="${e.target.offsetParent.href}">`,
